5889ad8c68391fced07f05f336a1349901ba8be8
aagavin <aagavin@gmail.com>
2018-03-11 18:04:31 -0400

    deleted requirments.txt added valid chain check added Consensus Algorithm

--- blockchain/objects/blockchain.py
+++ blockchain/objects/blockchain.py
@@ -1,7 +1,8 @@
 import hashlib
+import requests
 from time import time
 from typing import List, Dict
-
+from urllib.parse import urlparse
 import ujson as json
 
 
@@ -9,17 +10,19 @@ import ujson as json
 Transaction = Dict[str, str or int]
 Transactions = List[Transaction]
 
-Block = Dict[int, float or List[Transactions] or int or str]
+Block = Dict[int or float or List[Transactions] or int or str]
 Chain = List[Block]
 
 
 class Blockchain(object):
     def __init__(self):
-        self.chain = []
+        self.chain: Chain = []
         self.current_transactions: Transactions = []
 
         # Create a init block
         self.new_block(previous_hash=1, proof=100)
+
+        self.nodes = set()
 
     @property
     def last_block(self) -> Block:
@@ -28,6 +31,108 @@ class Blockchain(object):
         :return:
         """
         return self.chain[-1]
+
+    @staticmethod
+    def hash(block: Block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @staticmethod
+    def valid_proof(last_proof, proof) -> bool:
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain: Chain) -> bool:
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block: Block = chain[current_index]
+            # Verify that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check proof of work
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+
+        return False
+
+    def proof_of_work(self, last_proof):
+        """
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
 
     def new_block(self, proof, previous_hash=None) -> Block:
         """
@@ -62,38 +167,3 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
-
-    @staticmethod
-    def hash(block: Block):
-        """
-        Creates a SHA-256 hash of a Block
-        :param block: <dict> Block
-        :return: <str>
-        """
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    def proof_of_work(self, last_proof):
-        """
-
-        :param last_proof: <int>
-        :return: <int>
-        """
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
-
-        return proof
-
-    @staticmethod
-    def valid_proof(last_proof, proof) -> bool:
-        """
-        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
-        :param last_proof: <int> Previous Proof
-        :param proof: <int> Current Proof
-        :return: <bool> True if correct, False if not.
-        """
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
